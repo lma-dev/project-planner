749768d89a79798b3146e2c6290587c661248dac
lwinmoeaung-springarts <blackplayerbk@gmail.com>
2022-04-05 17:06:04 +0630

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import {
+  createRouter,
+  createWebHashHistory
+} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'home',
     component: HomeView
@@ -10,10 +12,6 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <template>
+  <h1>Home</h1>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="project in projects" :key="project.id">
+      <SingleProject :project="project"></SingleProject>
+    </div>
   </div>
 </template>
 
 <script>
+import SingleProject from '../components/SingleProject';
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  components: { SingleProject },
+  name: 'Home',
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then((response) => {
+        return response.json();
+      })
+      .then((datas) => {
+        this.projects = datas;
+      })
+      .catch((err) => {});
+  },
+};
 </script>
